a41012f366c26fdbf4ff789d41b50866111c43dd
dodobrid <ys1115112505@163.com>
2021-02-24 15:04:45 +0800

    class type transform

--- test_zh.cpp
+++ test_zh.cpp
@@ -21,6 +21,25 @@ int main()
 {
     SmallInt si(3);
     cout<< "si = " << si+3.14159 <<endl;
+
+    double dval= 2;
+    cout<< "si >= dval";
+    if(si >= dval)
+        cout<<"\tyes!"<<endl;
+    else
+        cout<<"\tno!"<<endl;
+
+    if(si)
+        cout<<"\tyes!"<<endl;
+    else
+        cout<<"\tno!"<<endl;
+
+    cout<<"si = "<<si<<endl;
+
+    int ival;
+    SmallInt s = 3.541;
+    ival = static_cast<int>(s) + 3;
+    cout<<"ival = "<< ival<<"\ts = "<<s<<endl;
 //    std::cout << "Hello world" << std::endl;
     return 0;
 }
